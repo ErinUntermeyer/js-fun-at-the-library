af32c6a8d701b94503d05a4d52816777521e27a9
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-05-10 17:59:37 -0600

    Add librarian exercises

--- src/librarian.js
+++ src/librarian.js
@@ -1,8 +1,39 @@
 class Librarian {
-  constructor() {
-
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
   }
-  
-}
+
+  greetPatron(patronName, boolean) {
+    if (boolean === true) {
+      return `Good morning, ${patronName}!`
+    } else {
+      return `Hello, ${patronName}!`
+    }
+  }
+
+  findBook(bookTitle) {
+    var shelfNames = Object.keys(this.library.shelves)
+
+    for (var i = 0; i < shelfNames.length; i++) {
+      var bookObjs = this.library.shelves[shelfNames[i]];
+      for (var j = 0; j < bookObjs.length; j++) {
+        var bookTitles = bookObjs[j].title;
+        var bookIndex = bookObjs.indexOf(bookObjs[j]);
+        if (bookTitle === bookTitles) {
+          bookObjs.splice(bookIndex, 1);
+          return `Yes, we have ${bookTitle}`
+        } else if (bookTitle !== bookTitles) {
+          return `Sorry, we do not have ${bookTitle}`
+        }
+      }
+    }
+  }
+
+  calculateLateFee(daysLate) {
+    return Math.ceil(daysLate * .25);
+  }
+
+};
 
 module.exports = Librarian;
